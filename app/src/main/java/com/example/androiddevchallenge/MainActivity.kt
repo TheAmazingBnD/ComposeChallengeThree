6043414c59ab48bc85f7c50f2013b87ab8e6c11e
theamazingbnd <bnd2524@gmail.com>
2021-10-25 21:43:54 -0400

    Pushing up what I have so folks can check it out. Lot's of improvements needed

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,19 +18,33 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import androidx.navigation.plusAssign
+import com.example.androiddevchallenge.home.Home
+import com.example.androiddevchallenge.navigation.NavigationActions
+import com.example.androiddevchallenge.navigation.Route
+import com.example.androiddevchallenge.network.models.data.ApiRedditPost
+import com.example.androiddevchallenge.redditpost.RedditPostBottomSheetLayout
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
+import com.google.accompanist.navigation.material.ModalBottomSheetLayout
+import com.google.accompanist.navigation.material.bottomSheet
+import com.google.accompanist.navigation.material.rememberBottomSheetNavigator
 
+@ExperimentalMaterialNavigationApi
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyTheme {
-                MyApp()
+                MyApp(true)
             }
         }
     }
@@ -38,24 +52,47 @@ class MainActivity : AppCompatActivity() {
 
 // Start building your app here!
 @Composable
-fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+@ExperimentalMaterialNavigationApi
+fun MyApp(darkTheme: Boolean) {
+    val navController = rememberNavController()
+    val navActions = remember(navController) { NavigationActions(navController) }
+
+    MyTheme(darkTheme = darkTheme) {
+        val bottomSheetNavigator = rememberBottomSheetNavigator()
+        navController.navigatorProvider += bottomSheetNavigator
+        ModalBottomSheetLayout(bottomSheetNavigator) {
+            NavHost(navController, startDestination = Route.Home.path) {
+                composable(Route.Home.path) {
+                    Home(
+                        darkTheme = darkTheme,
+                        openRedditPost = navActions.openDetail
+                    )
+                }
+                bottomSheet(Route.RedditPost.path) { backStackEntry ->
+                    val link = backStackEntry.arguments?.getString(Route.RedditPost.idArgument)
+                    requireNotNull(link)
+
+                    RedditPostBottomSheetLayout(darkTheme, link, navActions.navigateUp)
+                }
+            }
+        }
     }
 }
 
+@ExperimentalMaterialNavigationApi
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun LightPreview() {
-    MyTheme {
-        MyApp()
+    MyTheme(darkTheme = false) {
+        MyApp(false)
     }
 }
 
+@ExperimentalMaterialNavigationApi
 @Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun DarkPreview() {
     MyTheme(darkTheme = true) {
-        MyApp()
+        MyApp(true)
     }
 }
